5c2d893bf4879be313f99c97031b036fbd379acb
Robert Caw Brush <robertcawbrush@gmail.com>
2021-03-05 23:57:57 -0600

    added env and dev token

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import discord
+import os
+
 def main():
-    print('I am running in a container')
+
+    client = discord.Client()
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is ready')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        
+        if message.content.startswith('/hello'):
+            await message.channel.send('Good Evening')
+
+    client.run(os.getenv('COUNT_TOKEN'))
 
 if __name__ == '__main__':
     main()
