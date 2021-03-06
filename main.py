ced4718e092ae0bdcd9d33a5fac130f560e75a6a
Robert Caw Brush <robertcawbrush@gmail.com>
2021-03-06 00:42:50 -0600

    :beetle: trying to fix en race condition

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import discord
 import os
+import requests
+import json
 
 def main():
 
     client = discord.Client()
+    # TODO: Delete this
+    def get_quote():
+        response = requests.get("https://zenquotes.io/api.random")
+
+        json_data = json.load(response.text)
+
+        quote = json_data[0]['q'] + " -" + json_data[0]['a']
+        return quote
 
     @client.event
     async def on_ready():
@@ -16,8 +26,15 @@ def main():
         
         if message.content.startswith('/hello'):
             await message.channel.send('Good Evening')
+        
+        if message.content.startswith('/quote'):
+            quote = get_quote()
+            await message.channel.send(quote)
 
-    client.run(os.getenv('COUNT_TOKEN'))
-
+    try:
+        apikey = os.environ['COUNT_TOKEN']
+        client.run(apikey)
+    except KeyError as ke:
+        print(f'api key retrival failed for {ke}') 
 if __name__ == '__main__':
     main()
