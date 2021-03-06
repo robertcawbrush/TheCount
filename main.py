53f79108a292c5683a2ee1a627a166ee495007d9
Robert Caw Brush <robertcawbrush@gmail.com>
2021-03-06 16:20:51 -0600

    added error check for points val

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     load_dotenv()
 
     client = discord.Client()
+    
+    def get_args(body):
+        return body.split(' ')[1]
 
     @client.event
     async def on_ready():
@@ -23,6 +26,20 @@ def main():
 
         if message.author.bot or message.author == client.user:
             return
+        
+        if message.content.startswith(PREFIX + actions.POINTS):
+            try:
+                rg = get_args(message.content) 
+                points_to_add = int(rg)
+            except ValueError as ve:
+                await message.channel.send(f'error: supplied value {rg} is not a number')
+                return
+            username = f'{message.author.name}#{message.author.discriminator}'
+            # get drive doc
+            # get user that called
+            # for user that called add points to doc
+            # return point value added to user points, return current points after sum
+            return
 
         if message.content.startswith(PREFIX + actions.PING):
             await message.channel.send('Good Evening')
@@ -32,7 +49,10 @@ def main():
             available_commands = ''
             for command in actions.AVAILABLE_COMMANDS:
                 available_commands += PREFIX + command + '\n'
-            await message.channel.send('Available commands are : \n' + available_commands)
+            
+            response = 'All commands start with {} for example "-ping" \nAvailable commands are : \n{}\n some commands require your input such as a number for example:\n "{}"'.format(PREFIX, available_commands, (PREFIX + actions.POINTS + ' 3'))
+
+            await message.channel.send(response)
             return
 
         if message.content.startswith(PREFIX):
@@ -44,7 +64,8 @@ def main():
         client.run(apikey)
     except KeyError as ke:
         print(f'api key retrival failed for {ke}')
-
+    
+    
 
 if __name__ == '__main__':
     main()
